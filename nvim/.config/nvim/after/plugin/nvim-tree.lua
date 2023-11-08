f1dd27bbe2c183f167cb87ae26827bc82a2f6c1b
Vitalii Ohorodnik <v.ohorodnik@spd-ukraine.com>
2023-11-08 11:44:23 +0200

    Nvim update

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,53 +1,14 @@
--- examples for your init.lua
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
 
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
-
-local lib = require("nvim-tree.lib")
-local view = require("nvim-tree.view")
-
-local function collapse_all()
-    require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
-end
-
-local function edit_or_open()
-    -- open as vsplit on current node
-    local action = "edit"
-    local node = lib.get_node_at_cursor()
-
-    -- Just copy what's done normally with vsplit
-    if node.link_to and not node.nodes then
-        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-        view.close() -- Close the tree if file was opened
-    elseif node.nodes ~= nil then
-        lib.expand_or_collapse(node)
-    else
-        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-        view.close() -- Close the tree if file was opened
-    end
-end
-
-local function vsplit_preview()
-    -- open as vsplit on current node
-    local action = "vsplit"
-    local node = lib.get_node_at_cursor()
-
-    -- Just copy what's done normally with vsplit
-    if node.link_to and not node.nodes then
-        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-    elseif node.nodes ~= nil then
-        lib.expand_or_collapse(node)
-    else
-        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-    end
-
-    -- Finally refocus on tree if it was lost
-    view.focus()
+  -- Default mappings. Feel free to modify or remove as you wish.
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set("n", "l", api.node.open.edit, opts('Open'))
+  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts('Open'))
 end
 
 local HEIGHT_RATIO = 0.8
@@ -55,15 +16,6 @@ local WIDTH_RATIO = 0.5
 
 local config = {
     view = {
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = "l", action = "edit",           action_cb = edit_or_open },
-                { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
-                { key = "h", action = "close_node" },
-                { key = "H", action = "collapse_all",   action_cb = collapse_all }
-            }
-        },
         float = {
             enable = true,
             open_win_config = function()
@@ -97,10 +49,8 @@ local config = {
     },
     git = {
         ignore = false,
-    }
+    },
+    on_attach = my_on_attach,
 }
-
-vim.api.nvim_set_keymap("n", "<leader>pv", ":NvimTreeToggle<cr>",
-    { silent = true, noremap = true, desc = "Toggle NvimTree" })
 
 require('nvim-tree').setup(config)
--- nvim/.config/nvim/lua/ohorodnik/remap.lua
+++ nvim/.config/nvim/lua/ohorodnik/remap.lua
@@ -1,3 +1,4 @@
+local nvim_tree_api = require('nvim-tree.api')
 vim.g.mapleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection one line down" })
@@ -26,3 +27,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous warning"
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace" })
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true }, { desc = "Make file executable" })
+
+vim.keymap.set("n", "<leader>pv", nvim_tree_api.tree.toggle, { desc = "Open: New Tab" })
